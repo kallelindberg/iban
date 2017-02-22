8526cc195606008d6e96bc25bd6a37258d0c2979
Kalle Lindberg <kalle.lindberg@student.saimia.fi>
2017-02-22 12:10:45 +0200

    optimoitu

--- IbanCalc/Program.cs
+++ IbanCalc/Program.cs
@@ -11,11 +11,13 @@ namespace IbanCalc
         static string IbanCalc(string s)
         {
             int i = 0;
+            int mi = 0;
+            int ri = 0;
             bool result;
             int[] bban = new int[14];
             int[] acc = new int[14];
             int[] numero = new int[20];
-
+            int[] mtunnus = new int[6] { 1, 5, 1, 8, 0, 0 };
             int z = (14 - 6 - (s.Length - 6));
             for (int c = 0; c < s.Length; c++)
             {
@@ -25,11 +27,10 @@ namespace IbanCalc
                 else
                 i++;
             }
-            if (bban[0]== 4 || bban[0] == 5)
-            {
-                int ri = 0;
-                i = 0;
-                for (int c = 0; c < 7; c++)
+            i = 0;
+            if (bban[0] == 4 || bban[0] == 5){ mi = 7; }
+            else { mi = 6; }
+            for (int c = 0; c < mi; c++)
                 {
                     numero[i] = bban[ri];
                     i++;
@@ -46,39 +47,17 @@ namespace IbanCalc
                     i++;
                     ri++;
                 }
-            }
-            else
-            {
-                int ri = 0;
-                i = 0;
-                for (int c = 0; c < 6; c++)
-                {
-                    numero[i] = bban[ri];
-                    i++;
-                    ri++;
-                }
-                for (int c = 0; c < z; c++)
-                {
-                    numero[i] = 0;
-                    i++;
-                }
-                while (i < 14)
-                {
-                    numero[i] = bban[ri];
-                    i++;
-                    ri++;
-                }
-            }
             for (int c = 0; c < 14; c++)
             {
                 acc[c] = numero[c];
             }
-            numero[14] = 1;
-            numero[15] = 5;
-            numero[16] = 1;
-            numero[17] = 8;
-            numero[18] = 0;
-            numero[19] = 0;
+            i = 14;
+            for (int c = 0; c < 6;c++)
+            {
+                numero[i] = mtunnus[c];
+                i++; 
+            }
+
             string acn = string.Join("", numero);
             decimal jakoj = (decimal.Parse(acn) % 97);
             decimal tarkiste = 98 - jakoj;
@@ -88,7 +67,7 @@ namespace IbanCalc
         }
         static void Main(string[] args)
         {
-            string s = "123456-1234";
+            string s = "562009-4147118";
             string tulos = IbanCalc(s);
             Console.WriteLine(tulos);
             Console.ReadKey();
